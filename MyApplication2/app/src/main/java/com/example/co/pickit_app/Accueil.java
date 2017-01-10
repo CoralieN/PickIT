22ab500f6b38fa8f2a3d90a0955137f00466f009
jarrarines <jarrar@etud.insa-toulouse.fr>
2017-01-10 19:16:05 +0100

    connect DB working

--- MyApplication2/app/src/main/java/com/example/co/pickit_app/Accueil.java
+++ MyApplication2/app/src/main/java/com/example/co/pickit_app/Accueil.java
@@ -1,14 +1,11 @@
 package com.example.co.pickit_app;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
 
 public class Accueil extends AppCompatActivity {
 
@@ -36,6 +33,13 @@ public class Accueil extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+
+        if (id == R.id.action_connect) {
+            Intent intent = new Intent(Accueil.this, Connection.class);
+            startActivity(intent);
+            return true;
+        }
+
         if (id == R.id.action_list) {
             Intent intent = new Intent(Accueil.this, MyList.class);
             startActivity(intent);
@@ -61,10 +65,11 @@ public class Accueil extends AppCompatActivity {
         }
 
         if (id == R.id.action_account) {
-            Intent intent = new Intent(Accueil.this, Account.class);
+            Intent intent = new Intent(Accueil.this, Connection.class); // Intent intent = new Intent(Accueil.this, Account.class); a remplacer par ça et creer un nouveau IF!
             startActivity(intent);
             return true;
         }
+
 
 
         return super.onOptionsItemSelected(item);
--- MyApplication2/app/src/main/java/com/example/co/pickit_app/Connection.java
+++ MyApplication2/app/src/main/java/com/example/co/pickit_app/Connection.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.ksoap2.SoapEnvelope;
@@ -15,46 +14,35 @@ import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
-import java.util.ArrayList;
-
-public class add_obj extends AppCompatActivity {
+public class Connection extends AppCompatActivity {
 
     private final String NAMESPACE = "http://docs.insa.fr/";
     private final String URL = "http://192.168.43.191:8080/Localhost_official/Localhost3306Service?WSDL";
     private final String SOAP_ACTION = "http://docs.insa.fr/connect";
-    private final String METHOD_NAME = "add_obj";
+    private final String METHOD_NAME = "connect";
+    /** Called when the activity is first created. */
 
-    private Button scan = null;
-    private ImageButton validate = null;
-    Data data = new Data();
-    ArrayList<String> data_obj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_obj);
+        setContentView(R.layout.activity_connection);
+        System.out.println("coucou1");
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //scan = (Button) findViewById(R.id.scanbutton);
-        validate =(ImageButton) findViewById(R.id.validate_new_obj);
-       /* scan.setOnClickListener(new View.OnClickListener() {
-            @Override
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.button_connect);
+        fab.setOnClickListener(new View.OnClickListener() {*/
+
+            Button login = (Button) findViewById(R.id.button_connect);
+            login.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View view) {
-                Snackbar.make(view, "Open the Scanner ", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+               /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
 
-
-        //Completer ce clickListener pour ajouter un objet dans la table objects
-        validate.setOnClickListener(new View.OnClickListener() {
-
-            EditText name = (EditText) findViewById(R.id.fillnameobj);
-
-            @Override
-            public void onClick(View view) {
-                data.add_list(name.getText().toString());
 
                 Thread networkThread = new Thread() {
                     @Override
@@ -62,18 +50,28 @@ public class add_obj extends AppCompatActivity {
 
                         try {
 
+
                             System.out.println("coucou2");
                             SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
 
                             System.out.println("coucou3");
 
-                            String Name = name.getText().toString(); //retreive le texte tapé sur l'appli
-                            System.out.println("obj_name !!!! " + Name);
+                            EditText userName = (EditText) findViewById(R.id.Login);
+                            String user_Name = userName.getText().toString(); //retreive le texte tapé sur l'appli
+                            System.out.println("username !!!! "+user_Name);
+
+                            EditText userPassword = (EditText) findViewById(R.id.Pwd);
+                            String user_Password = userPassword.getText().toString(); // recupérer pswd tappé sur appli
+                            System.out.println("user password !!!! "+user_Password);
 
 
+                            System.out.println("coucou4");
 
-                            request.addProperty("arg0", Name);
+                            System.out.println("coucou5");
 
+
+                            request.addProperty("arg0", user_Name);
+                            request.addProperty("arg1",user_Password);
 
                             System.out.println("coucou6");
 
@@ -83,6 +81,10 @@ public class add_obj extends AppCompatActivity {
                             HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
 
                             androidHttpTransport.debug = true;
+
+                            System.out.println("coucou7");
+
+
 
                             System.out.println("coucou8");
                             androidHttpTransport.call(SOAP_ACTION, envelope);
@@ -118,11 +120,6 @@ public class add_obj extends AppCompatActivity {
             }
         });
 
-
-
-
-                /*Intent intent = new Intent(add_obj.this, MyObj.class);
-                startActivity(intent);*/
-            }
+    }
 
 }
